3c5b82d1aa6fa4975188e7d668a5eb76a77508a8
jerome <805841744@qq.com>
2019-11-16 16:18:55 +0800

    修改

--- app/middleware/xmlparser.ts
+++ app/middleware/xmlparser.ts
@@ -1,7 +1,7 @@
 export default options => {
   return async function gzip(ctx, next) {
     const xmlparser = require('express-xml-bodyparser');
-    const { request, response, } = ctx;
-    return xmlparser(options)(request, response, next);
+    const { req, res, } = ctx;
+    return xmlparser(options)(req, res, next);
   };
 };
